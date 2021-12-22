a51fcef58a1a3687c74fbd1b0c464c61fe9fd56c
ga-almeida <gabrielsantos.almeida@hotmail.com>
2021-12-22 19:58:10 -0300

    add fidnOne

--- src/create.ts
+++ src/create.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-    // Criar uma informação no banco de dados
     const result = await prisma.courses.create({
         data: {
             duration: 200,
