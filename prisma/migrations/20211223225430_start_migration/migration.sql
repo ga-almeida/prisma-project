b18f179393eea92e28b2380e259cf5d26c69d253
ga-almeida <gabrielsantos.almeida@hotmail.com>
2021-12-23 19:55:38 -0300

    update migrations

--- prisma/migrations/20211223225430_start_migration/migration.sql
+++ prisma/migrations/20211223225430_start_migration/migration.sql
@@ -15,8 +15,17 @@ CREATE TABLE "courses" (
     "description" TEXT,
     "duration" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "teacher_id" TEXT NOT NULL,
 
     CONSTRAINT "courses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teachers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "teachers_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -24,3 +33,12 @@ CREATE UNIQUE INDEX "modules_name_key" ON "modules"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "courses_name_key" ON "courses"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "courses_teacher_id_key" ON "courses"("teacher_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teachers_name_key" ON "teachers"("name");
+
+-- AddForeignKey
+ALTER TABLE "courses" ADD CONSTRAINT "courses_teacher_id_fkey" FOREIGN KEY ("teacher_id") REFERENCES "teachers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
